f5f9cc448362aa2d67555f6643ffa3f3f2aa83d3
Areeb0206 <masafvi48@gmail.com>
2021-03-17 22:12:13 +0530

    first commit

--- src/spa/Projects/Projects.js
+++ src/spa/Projects/Projects.js
@@ -20,7 +20,7 @@ export default function Projects() {
 
                         <h1><span className="dot">.</span>project<span className="bracket">( )</span></h1>
                         
-                        <p>works On</p>
+                        <p>recent  projects</p>
 
 
 
@@ -29,9 +29,23 @@ export default function Projects() {
                      <div className="top-project">
 
                           <div className="project1">
-                              <div className="project1-left"></div>
+                              <div className="project1-left">
+                              
 
-                              <div className="project1-right"></div>
+                                  <h3>Project - 1</h3>
+           
+                                  <h4>Pubg-Mobile</h4>
+                                  <p>The first and most extraordinary Battle Royale, better than the original. And thats why i have created this websiteso that anyyone can download this game  </p>
+
+                              </div>
+
+                              <div className="project1-right">
+
+                                       <div className="link1-image"></div>
+
+                                       <div className="link1"><a href="https://areeb0206.github.io/pubg">https://areeb0206.github.io/pubg</a></div>
+
+                              </div>
 
 
 
@@ -39,18 +53,44 @@ export default function Projects() {
 
                           <div className="project2">
 
-                             <div className="project2-left"></div>
+                             <div className="project2-left">
 
-                              <div className="project2-right"></div>
+
+                                    <div className="link2-image"></div>
+                                    <div className="link2">
+                                     <a href="https://areeb0206.github.io/single-page-application/">https://areeb0206.github.io/single-page-application/</a></div>
+                                     </div>
+
+                              <div className="project2-right"> 
+                                <h3>Project - 2</h3>
+           
+                                  <h4>Hospital</h4>
+                                  <p>Its a SPA (single page application) Hospital website , From where anyone can get appointment and contact us for any queries</p></div>
 
 
                           </div>
 
+
+                          
+
                           <div className="project3">
 
-                              <div className="project3-left"></div>
+                              <div className="project3-left">  
+                               <h3>Project - 3</h3>
+           
+                                  <h4>Hospital Dashboard</h4>
+                                  <p>Its not a responsive website (only Desktop View) . It enables healthcare professionals to access important patient statistics in real-time to increase the overall hospital performance and patient satisfaction.</p>
 
-                              <div className="project3-right"></div>
+
+                              </div>
+
+                              <div className="project3-right">
+                              <div className="link3-image"></div>
+                              <div className="link3">
+                              <a href="https://coronacovid.herokuapp.com/">https://coronacovid.herokuapp.com/</a></div>
+
+
+                              </div>
 
 
 
